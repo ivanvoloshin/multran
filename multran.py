9cafa92073285e24a3229e06bbcfc51c72a059f4
Ivan Voloshin <8472117+vanyavoloshin@users.noreply.github.com>
2023-04-07 00:41:09 +0300

    formatted with black

--- multran.py
+++ multran.py
@@ -7,31 +7,25 @@ from sys import exit
 from bs4 import BeautifulSoup
 
 parser = argparse.ArgumentParser(
-    description='English word transcription fetching tool',
-    usage='%(prog)s'
+    description="English word transcription fetching tool", usage="%(prog)s"
 )
 
 parser.add_argument(
-    'word',
-    metavar='WORD',
-    type=str,
-    help='Specify word',
-    action='store',
-    nargs='?'
+    "word", metavar="WORD", type=str, help="Specify word", action="store", nargs="?"
 )
 
 args = parser.parse_args()
 
 
-class Multran():
+class Multran:
     def __init__(self, word=None) -> None:
         self.word = word
 
     def process(self, word):
         if len(word.split()) > 1:
-            word = word.replace(' ', '+')
+            word = word.replace(" ", "+")
 
-        url = f'https://www.multitran.com/m.exe?l1=1&l2=2&s={word}&langlist=2'
+        url = f"https://www.multitran.com/m.exe?l1=1&l2=2&s={word}&langlist=2"
 
         try:
             response = get(url)
@@ -39,21 +33,21 @@ class Multran():
             exit(e)
 
         page = response.content
-        soup = BeautifulSoup(page, 'html.parser')
-        data = soup.findAll('span', attrs={'style': 'color:gray'})
+        soup = BeautifulSoup(page, "html.parser")
+        data = soup.findAll("span", attrs={"style": "color:gray"})
 
         if len(data) == 7:
-            exit('Word not found')
+            exit("Word not found")
 
         try:
             self.find_transcription(1, data)
         except IndexError:
-            exit('Transcription not found')
+            exit("Transcription not found")
 
     def find_transcription(self, index, lst):
         transcription = lst[index].text
 
-        if transcription.startswith('['):
+        if transcription.startswith("["):
             pyperclip.copy(transcription)
             print(transcription)
         else:
@@ -67,7 +61,7 @@ if args.word:
 else:
     try:
         while 1:
-            word = input('Enter word: ')
+            word = input("Enter word: ")
             Multran().process(word)
             print()
     except KeyboardInterrupt as e:
